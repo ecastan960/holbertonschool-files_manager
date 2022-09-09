815bb8ead0c8536513e6b3f393d8e1752dc6c263
ecastan960 <ecastan1@eafit.edu.co>
2022-09-08 22:40:59 -0500

    [05] task 4

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,32 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
-import sha1 from 'sha1';
+import crypto from 'crypto';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AuthController {
   static async getConnect(request, response) {
-    let credential = request.header('Authorization');
-    if (!credential) {
-      response.status(400).json({ error: 'Unauthorized' });
-      return {};
-    }
-    credential = credential.slice(6);
-    const string = Buffer.from(credential, 'base64');
-    const [email, password] = string.toString('utf8').split(':');
-    const user = await dbClient.findUser({ email });
-
-    if (!user) {
-      response.status(401).json({ error: 'Unauthorized' });
-      return {};
-    }
-    if (sha1(password) !== user.password) {
-      response.status(403).json({ error: 'Invalid credentials' });
-      return {};
-    }
+    let authHeader = request.headers.authorization;
+    authHeader = authHeader.slice(6);
+    const credential = Buffer.from(authHeader, 'base64').toString();
+    if (!credential.includes(':')) return null;
+    const [email, password] = credential.toString('utf8').split(':');
+    const hashedPassword = crypto.createHash('SHA1').update(password).digest('hex');
+    if (!email) return response.status(401).json({ error: 'Unauthorized' });
+    const user = await dbClient.users.find({ email, password: hashedPassword }).toArray();
+    if (!user) return response.status(401).json({ error: 'Unauthorized' });
     const token = uuidv4();
     const key = `auth_${token}`;
-    await redisClient.set(key, user._id.toString(), 86400);
-    return response.status(200).json({ token });
+    await redisClient.set(key, user._id.toString(), 60 * 60 * 24);
+    return response.json({ token });
   }
 
   static async getDisconnect(request, response) {
